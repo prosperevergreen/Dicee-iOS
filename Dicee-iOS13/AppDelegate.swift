c3c4a641f90da45013a359739f8de831490d38d2
prosperevergreen <prosperevergreen@gmail.com>
2020-06-21 03:32:46 +0300

    corrected the naming

--- Dicee-iOS13/AppDelegate.swift
+++ Dicee-iOS13/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  Dicee-iOS13
 //
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by Prosper Evergreen on 21/06/2020.
+//  Copyright © 2020. All rights reserved.
 //
 
 import UIKit
